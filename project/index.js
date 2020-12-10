74c7b1cdfd9ed43ebdee1590a381d6ddc86d4a2e
victorgbn <49316233+victorgbn@users.noreply.github.com>
2020-12-10 16:57:57 +0100

    Build and add static folder

--- project/index.js
+++ project/index.js
@@ -13,5 +13,14 @@ app.use(cors());
 const router = require('./routes/router.js');
 app.use('/api', router);
 
+//Handle production
+if(process.env.NODE_ENV === 'production'){
+    //static folder
+    app.use(express.static(__dirname + '/public/'));
+
+    //Handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 // run server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- vue-foot/vue.config.js
+++ vue-foot/vue.config.js
@@ -1,6 +1,15 @@
+const path = require('path')
+
 module.exports = { 
-    // options ... 
+    outputDir: path.resolve(__dirname, '../project/public'),
     devServer: { 
-        proxy: 'https://www.thesportsdb.com/', 
+        proxy: {
+            '/api':{
+                target: 'http://localhost:8000'
+            }
+            // 'api/v1/json/':{
+            //     target : 'https://www.thesportsdb.com'
+            // }
+        }
     }
 }
